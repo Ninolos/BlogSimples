383c32159224cc04766515b03205464a850d2997
Jonathan Daniel Loth <ninoloth@gmail.com>
2022-03-02 15:29:42 -0300

    Backend Logic development
    Adding DB string connection
    add Create and Edit logic from CRUD

---
+++ BlogPessoal/BlogPessoal.Web/Data/Contexto/BlogPessoalContexto.cs
@@ -0,0 +1,26 @@
+﻿using BlogPessoal.Web.Data.Mapeamento;
+using BlogPessoal.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BlogPessoal.Web.Data.Contexto
+{
+    public class BlogPessoalContexto : DbContext
+    {
+        public BlogPessoalContexto() : base(typeof(BlogPessoalContexto).Name)
+        {
+
+        }
+
+        public DbSet<CategoriaDeArtigo> CategoriasDeArtigo { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new CategoriaDeArtigoMap());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
